c90d55759667a51373e5d477be3ea44ba0d37c81
Slobodan Ilic <slobodan@crunch.io>
2019-05-27 23:49:48 +0200

    Add basic reshaping for CAT x CAT

--- src/JsCube.js
+++ src/JsCube.js
@@ -1,4 +1,90 @@
-function JsCube(response) {
-    this.result = response.result;
+var nj = require('numjs');
+
+const dimensionTypes = Object.freeze({
+  CAT: 0,
+  MR: 1,
+});
+
+class Dimension {
+  constructor(dimDict) {
+    this.dimDict = dimDict;
+  }
+
+  get type() {
+    return this.getType();
+  }
+
+  get categories() {
+    return this.dimDict.type.categories;
+  }
+
+  get validIndices() {
+    return this.categories
+      .map((e, i) => (!e.missing ? i : -1))
+      .filter(i => i !== -1);
+  }
+
+  getType() {
+    let typeClass = this.dimDict.type.class;
+    if (typeClass === 'categorical') {
+      return dimensionTypes.CAT;
+    }
+  }
 }
-module.exports = JsCube;
+
+class JsCube {
+  constructor(response) {
+    this.result = response.result;
+  }
+
+  get counts() {
+    return nj.array(this.result.counts).reshape(this.shape);
+  }
+
+  get dimensions() {
+    return this.result.dimensions.map(dimDict => new Dimension(dimDict));
+  }
+
+  get dimensionTypes() {
+    return this.dimensions.map(dim => dim.type);
+  }
+
+  get slices() {
+    return this.getSlices();
+  }
+
+  get shape() {
+    return this.dimensions.map(d => d.categories.length);
+  }
+
+  getSlices() {
+    if (this.dimensions.length === 2) {
+      return [new CatXCatMatrix(this.dimensions, this.counts)];
+    } else {
+      // TODO: Index correct counts
+    }
+  }
+  // this.getCounts = () => {};
+  // this.counts = this.getCounts();
+}
+
+class CatXCatMatrix {
+  constructor(dimensions, counts) {
+    this.dimensions = dimensions;
+    this.counts = counts;
+  }
+
+  get columnMargin() {
+    return this.getColumnMargin();
+  }
+
+  getColumnMargin() {
+    let counts = this.counts;
+  }
+}
+
+module.exports = {
+  JsCube: JsCube,
+  dimensionTypes: dimensionTypes,
+  Dimension: Dimension,
+};
--- tests/JsCube.test.js
+++ tests/JsCube.test.js
@@ -1,9 +1,61 @@
-const JsCube = require('../src/JsCube');
+const nj = require('numjs');
+const jsc = require('../src/JsCube');
+const cat_x_cat = require('./fixtures/cat-x-cat.json');
+const DT = jsc.dimensionTypes;
+const catXCatCube = new jsc.JsCube(cat_x_cat);
 
 describe('JsCube', () => {
-    test('loads response JSON', () => {
-        let response = require('./fixtures/cat-x-cat.json');
-        let cube = new JsCube(response);
-        expect(cube.result.counts).toEqual([5, 3, 2, 0, 5, 2, 3, 0, 0, 0, 0, 0]);
-    });
+  it('loads response JSON', () => {
+    expect(catXCatCube.result.counts).toEqual([
+      5,
+      3,
+      2,
+      0,
+      5,
+      2,
+      3,
+      0,
+      0,
+      0,
+      0,
+      0,
+    ]);
+  });
+
+  it('resolves CAT x CAT types', () => {
+    expect(catXCatCube.dimensionTypes).toEqual([
+      jsc.dimensionTypes.CAT,
+      jsc.dimensionTypes.CAT,
+    ]);
+  });
+
+  it('knows its shape with all elements', () => {
+    expect(catXCatCube.shape).toEqual([3, 4]);
+  });
+
+  it('reshapes counts', () => {
+    console.log(catXCatCube.counts);
+    expect(catXCatCube.counts).toEqual(
+      nj.array([[5, 3, 2, 0], [5, 2, 3, 0], [0, 0, 0, 0]]),
+    );
+  });
+});
+
+describe('Dimension', () => {
+  it('resolves to CAT type', () => {
+    let dimension = new jsc.Dimension(cat_x_cat.result.dimensions[0]);
+    expect(dimension.type).toEqual(DT.CAT);
+  });
+
+  it('knows its valid indexes', () => {
+    let dimension = new jsc.Dimension(cat_x_cat.result.dimensions[0]);
+    expect(dimension.validIndices).toEqual([0, 1]);
+  });
+});
+
+describe.skip('CatXCatMatrix', () => {
+  it('calculates column margin', () => {
+    let matrix = new jsc.JsCube(cat_x_cat).slices[0];
+    expect(matrix.columnMargin).toEqual([1, 2, 3]);
+  });
 });
